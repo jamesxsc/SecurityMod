f9c7a9acdaa1a9c9904b3243b22a3d813e3f572c
jamesxsc <james@xsc.co.uk>
2019-06-07 20:40:28 +0100

    validity checks

--- src/main/java/uk/co/xsc/securitymod/block/CameraBlock.java
+++ src/main/java/uk/co/xsc/securitymod/block/CameraBlock.java
@@ -4,6 +4,7 @@ import com.mojang.blaze3d.platform.GlStateManager;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.block.FabricBlockSettings;
+import net.minecraft.ChatFormat;
 import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.client.MinecraftClient;
@@ -12,6 +13,7 @@ import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.util.NarratorManager;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.network.chat.BaseComponent;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.Style;
 import net.minecraft.network.chat.TextComponent;
@@ -105,10 +107,47 @@ public class CameraBlock extends FacingBlock implements BlockEntityProvider, INe
     @Environment(EnvType.CLIENT)
     public Screen getConfigurationScreen(BlockEntity cameraBlockEntity) {
         Screen screen = new Screen(NarratorManager.EMPTY) {
+
             private final BlockEntity blockEntity;
             private int sliderWidth = 150;
             private int baseY;
             private int windowHeight;
+
+            private FilteredTextFieldWidget addressBox1;
+            private FilteredTextFieldWidget addressBox2;
+            private FilteredTextFieldWidget addressBox3;
+            private FilteredTextFieldWidget addressBox4;
+
+            private Component addressValidity() {
+                if (
+                        addressBox1.getText().length() == 0 ||
+                                addressBox2.getText().length() == 0 ||
+                                addressBox3.getText().length() == 0 ||
+                                addressBox4.getText().length() == 0
+                ) {
+                    return new TextComponent(" (Invalid)").setStyle(new Style().setColor(ChatFormat.DARK_RED));
+                } else {
+                    return new TextComponent(" (Valid)").setStyle(new Style().setColor(ChatFormat.DARK_GREEN));
+                }
+            }
+
+            private FilteredTextFieldWidget scopeBox1;
+            private FilteredTextFieldWidget scopeBox2;
+            private FilteredTextFieldWidget scopeBox3;
+            private FilteredTextFieldWidget scopeBox4;
+
+            private Component scopeValidity() {
+                if (
+                        scopeBox1.getText().length() == 0 ||
+                                scopeBox2.getText().length() == 0 ||
+                                scopeBox3.getText().length() == 0 ||
+                                scopeBox4.getText().length() == 0
+                ) {
+                    return new TextComponent(" (Invalid)").setStyle(new Style().setColor(ChatFormat.DARK_RED));
+                } else {
+                    return new TextComponent(" (Valid)").setStyle(new Style().setColor(ChatFormat.DARK_GREEN));
+                }
+            }
 
             {
                 this.blockEntity = cameraBlockEntity;
@@ -152,19 +191,19 @@ public class CameraBlock extends FacingBlock implements BlockEntityProvider, INe
                 int addressBoxWidth = 27;
                 int[] oldAddressValues = ArrayUtil.intFromStr(((CameraBlockEntity) blockEntity).getAddress().split("\\."), false);
 
-                FilteredTextFieldWidget addressBox1 = new FilteredTextFieldWidget(this.font, this.width / 2 - (addressBoxWidth * 2 + 5), baseY + 132, addressBoxWidth, 15, "",
+                addressBox1 = new FilteredTextFieldWidget(this.font, this.width / 2 - (addressBoxWidth * 2 + 5), baseY + 132, addressBoxWidth, 15, "",
                         (Predicate<String>) s -> s.matches("^[0-9]*$"));
                 addressBox1.setSuggestion("192");
                 addressBox1.setMaxLength(3);
-                FilteredTextFieldWidget addressBox2 = new FilteredTextFieldWidget(this.font, this.width / 2 - (addressBoxWidth * 1 + 0), baseY + 132, addressBoxWidth, 15, "",
+                addressBox2 = new FilteredTextFieldWidget(this.font, this.width / 2 - (addressBoxWidth * 1 + 0), baseY + 132, addressBoxWidth, 15, "",
                         (Predicate<String>) s -> s.matches("^[0-9]*$"));
                 addressBox2.setSuggestion("168");
                 addressBox2.setMaxLength(3);
-                FilteredTextFieldWidget addressBox3 = new FilteredTextFieldWidget(this.font, this.width / 2 + (addressBoxWidth * 0 + 5), baseY + 132, addressBoxWidth, 15, "",
+                addressBox3 = new FilteredTextFieldWidget(this.font, this.width / 2 + (addressBoxWidth * 0 + 5), baseY + 132, addressBoxWidth, 15, "",
                         (Predicate<String>) s -> s.matches("^[0-9]*$"));
                 addressBox3.setSuggestion("1");
                 addressBox3.setMaxLength(3);
-                FilteredTextFieldWidget addressBox4 = new FilteredTextFieldWidget(this.font, this.width / 2 + (addressBoxWidth * 1 + 10), baseY + 132, addressBoxWidth, 15, "",
+                addressBox4 = new FilteredTextFieldWidget(this.font, this.width / 2 + (addressBoxWidth * 1 + 10), baseY + 132, addressBoxWidth, 15, "",
                         (Predicate<String>) s -> s.matches("^[0-9]*$"));
                 addressBox4.setSuggestion("135");
                 addressBox4.setMaxLength(3);
@@ -183,19 +222,19 @@ public class CameraBlock extends FacingBlock implements BlockEntityProvider, INe
 
                 int[] oldScopeValues = ArrayUtil.intFromStr(((CameraBlockEntity) blockEntity).getScope().split("\\."), true);
 
-                FilteredTextFieldWidget scopeBox1 = new FilteredTextFieldWidget(this.font, this.width / 2 - (addressBoxWidth * 2 + 5), baseY + 164, addressBoxWidth, 15, "",
+                scopeBox1 = new FilteredTextFieldWidget(this.font, this.width / 2 - (addressBoxWidth * 2 + 5), baseY + 164, addressBoxWidth, 15, "",
                         s -> s.matches("^[xX0-9]*$"));
                 scopeBox1.setSuggestion("192");
                 scopeBox1.setMaxLength(3);
-                FilteredTextFieldWidget scopeBox2 = new FilteredTextFieldWidget(this.font, this.width / 2 - (addressBoxWidth * 1 + 0), baseY + 164, addressBoxWidth, 15, "",
+                scopeBox2 = new FilteredTextFieldWidget(this.font, this.width / 2 - (addressBoxWidth * 1 + 0), baseY + 164, addressBoxWidth, 15, "",
                         s -> s.matches("^[xX0-9]*$"));
                 scopeBox2.setSuggestion("168");
                 scopeBox2.setMaxLength(3);
-                FilteredTextFieldWidget scopeBox3 = new FilteredTextFieldWidget(this.font, this.width / 2 + (addressBoxWidth * 0 + 5), baseY + 164, addressBoxWidth, 15, "",
+                scopeBox3 = new FilteredTextFieldWidget(this.font, this.width / 2 + (addressBoxWidth * 0 + 5), baseY + 164, addressBoxWidth, 15, "",
                         s -> s.matches("^[xX0-9]*$"));
                 scopeBox3.setSuggestion("1");
                 scopeBox3.setMaxLength(3);
-                FilteredTextFieldWidget scopeBox4 = new FilteredTextFieldWidget(this.font, this.width / 2 + (addressBoxWidth * 1 + 10), baseY + 164, addressBoxWidth, 15, "",
+                scopeBox4 = new FilteredTextFieldWidget(this.font, this.width / 2 + (addressBoxWidth * 1 + 10), baseY + 164, addressBoxWidth, 15, "",
                         s -> s.matches("^[xX0-9]*$"));
                 scopeBox4.setSuggestion("x");
 
@@ -258,9 +297,14 @@ public class CameraBlock extends FacingBlock implements BlockEntityProvider, INe
                 this.drawCenteredString(this.font, new TextComponent("Tilt").getFormattedText(), this.width / 2, baseY + 65, 16777215);
 
                 //network settings
-                this.drawCenteredString(this.font, new TextComponent("Network Settings").setStyle(new Style().setUnderline(true)).getFormattedText(), this.width / 2, baseY + 105, 16777215);
-                this.drawCenteredString(this.font, new TextComponent("Device Address").getFormattedText(), this.width / 2, baseY + 120, 16777215);
-                this.drawCenteredString(this.font, new TextComponent("Scope").getFormattedText(), this.width / 2, baseY + 152, 16777215);
+                this.drawCenteredString(this.font, new TextComponent("Network Settings").setStyle(new Style().setUnderline(true))
+                        .getFormattedText(), this.width / 2, baseY + 105, 16777215);
+                this.drawCenteredString(this.font, new TextComponent("Device Address")
+                        .append(addressValidity())
+                        .getFormattedText(), this.width / 2, baseY + 120, 16777215);
+                this.drawCenteredString(this.font, new TextComponent("Scope")
+                        .append(scopeValidity())
+                        .getFormattedText(), this.width / 2, baseY + 152, 16777215);
 
                 super.render(int_1, int_2, float_1);
             }
